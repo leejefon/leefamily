0ea6aff22654c1aaa9b8b29f0a8bbfe166b7c5d3
Jeff Lee <jeff@healthiq.com>
2017-12-20 15:01:31 -0800

    More misc updates

--- test/services/classes.test.js
+++
@@ -1,9 +0,0 @@
-const assert = require('assert');
-const app = require('../../src/app');
-
-describe('\'classes\' service', () => {
-  it('registered the service', () => {
-    const service = app.service('classes');
-    assert.ok(service, 'Registered the service');
-  });
-});
